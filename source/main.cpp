f65f52fb64e041d293c6278e4c853d000baa3191
Denis Cooper <denis.cooper09@gmail.com>
2023-05-31 15:54:39 +0100

    Implement perlin noise algorithm using C for generating maps

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 
 #include "tile_map.h"
+#include "perlin_noise.h"
 
 int main() {
     sf::ContextSettings settings;
@@ -17,7 +18,7 @@ int main() {
 
     std::random_device rd;
     std::mt19937 mt(rd());
-    std::uniform_int_distribution dist(0, 4);
+    std::uniform_int_distribution dist(0, 100);
 
     const uint32_t TILE_MAP_HEIGHT = 50;
     const uint32_t TILE_MAP_WIDTH = 75;
@@ -26,7 +27,17 @@ int main() {
     for (uint32_t y = 0; y < TILE_MAP_HEIGHT; ++y) {
         map[y] = new uint32_t[TILE_MAP_WIDTH];
         for (uint32_t x = 0; x < TILE_MAP_WIDTH; ++x) {
-            map[y][x] = dist(mt);
+            double noise = perlin_noise_2_d(static_cast<float>(x),
+                                            static_cast<float>(y),
+                                            0.1,
+                                            2,
+                                            2009);
+
+            if (noise < 0.5f) {
+                map[y][x] = dist(mt) < 90 ? 1 : dist(mt) < 50 ? 3 : dist(mt) < 50 ? 4 : 2;
+            } else {
+                map[y][x] = 0;
+            }
         }
     }
 
---
+++ source/perlin_noise.h
@@ -0,0 +1,14 @@
+#ifndef CAT_FIGHTER_PERLIN_NOISE_H
+#define CAT_FIGHTER_PERLIN_NOISE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__((unused)) extern double perlin_noise_2_d(double x, double y, double frequency, int depth, int seed);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
